fbce0d3e2bbaae8ed615b70b3d4906d74daa7e30
Haifeng Shen <137964267+haifshen@users.noreply.github.com>
2024-10-31 18:34:28 +1100

    Update calculator class
    Read the expression from keyboard. Design a simple algorithm....

--- Activity-1.6/src/Calculator.java
+++ Activity-1.6/src/Calculator.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        // 1.2 + 3.4
+        String expression = input.nextLine();
+
+        String[] tokens = expression.split(" ");
+
+        double op1 = Double.parseDouble(tokens[0]);
+        String op = tokens[1];
+        double op2 = Double.parseDouble(tokens[2]);
+
+        double result = 0;
+
+        switch (op) {
+            case "+" -> result = op1 + op2;
+            case "-" -> result = op1 - op2;         
+        }
+
+        System.out.println(result);
     }
 }
